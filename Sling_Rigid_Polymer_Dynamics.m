f173eb6d2b2c813ad82555956c3cf422d463920f
doerlbh <doerlbh@gmail.com>
2016-07-21 15:29:50 -0700

    Update code

--- Sling_Rigid_Polymer_Dynamics.m
+++ Sling_Rigid_Polymer_Dynamics.m
@@ -1,6 +1,16 @@
-% Sling_Rigid_Polymer_Dynamics
+% Sling_Rigid_Polymer_Dynamics_2D
 % Baihan Lin, Qian Lab
 % July 2016
 
 % To simulate the dynamics of a rigid polymer by Sling model
+
+trial = 1; % training
+twist = 100; % change of state
+node = 8; % nodes of rigid polymer
+angle = 0.02 % in rad, angle changed in each twist
+
+
+state 
+
+
 
