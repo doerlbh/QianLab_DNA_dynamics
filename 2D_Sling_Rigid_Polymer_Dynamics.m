cd35f9f073d27710ad5f7dfad42e4b7140938a8b
doerlbh <doerlbh@gmail.com>
2016-07-21 15:36:00 -0700

    initialization

---
+++ 2D_Sling_Rigid_Polymer_Dynamics.m
@@ -0,0 +1,26 @@
+% Sling_Rigid_Polymer_Dynamics_2D
+% Baihan Lin, Qian Lab
+% July 2016
+
+% To simulate the dynamics of a rigid polymer by Sling model
+
+trial = 1; % training
+twist = 100; % change of state
+node = 8; % nodes of rigid polymer
+
+angle = 0.02; % in rad, angle changed in each twist
+L = 1; % length of each segment of rigid polymer
+a = 10; % threshold to form loop
+
+Hc = 1.0 % energy level of cis rigid configuration
+Ht = 0.9 % energy level of trans rigid configuration
+
+% generate 
+
+
+
+
+
+
+
+
