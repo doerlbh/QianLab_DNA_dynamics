fa306e95b16b89244e368c7ae3e8fa99bc18d8cc
doerlbh <doerlbh@gmail.com>
2016-08-19 08:57:50 -0700

    update code

--- BL_hist_reader.m
+++ BL_hist_reader.m
@@ -8,11 +8,11 @@ close all;
 
 %% pEf
 
-pEf = dlmread('/Users/DoerLBH/Dropbox/git/QianLab_DNA_dynamics/lab_sim/data/data_20160818/');
+pEf = dlmread('/Users/DoerLBH/Dropbox/git/QianLab_DNA_dynamics/lab_sim/data/data_20160818/rEquil-E-N200-t2000-a20-l1-r0.02.txt');
 
 fig1 = figure;
-histogram(pEf, 'BinWidth', 1);
-title('Energy Histogram in rEquilibrium for N  -t  -a  -l -r  ');
+histogram(pEf, 'BinWidth', 0.25);
+title('Energy Histogram in rEquilibrium for N200-t2000-a20-l1-r0.02');
 xc = xlim;
 xl = xc(1)*0.2+xc(2)*0.8;
 yc = ylim;
@@ -20,17 +20,17 @@ yl1 = yc(1)*0.17+yc(2)*0.83;
 yl2 = yc(1)*0.23+yc(2)*0.77;
 text(xl,yl1,strcat('mean=',num2str(mean(pEf))),'Color','red','FontSize',12);
 text(xl,yl2,strcat('var=',num2str(var(pEf))),'Color','red','FontSize',12);
-filename = strcat(fpathN, 'rEquil-E-Hist-N',num2str(fnode),'-t',num2str(ftwist),'-a',num2str(fa),'-l',num2str(fL),'-r',num2str(fangle),'.png');
+filename = 'rEquil-E-N200-t2000-a20-l1-r0.02.png';
 parsaveas(gcf, filename,'png');
 close gcf;
 
 %% pDf
 
-pDf = dlmread('/Users/DoerLBH/Dropbox/git/QianLab_DNA_dynamics/lab_sim/data/data_20160818/');
+pDf = dlmread('/Users/DoerLBH/Dropbox/git/QianLab_DNA_dynamics/lab_sim/data/data_20160818/Equil-D-N200-A2000-a20-l1-r0.02.txt');
 
 fig2 = figure;
-histogram(pDf, 'BinWidth', 20);
-title('HTdistance Histogram in rEquilibrium for N  -t  -a  -l -r  ');
+histogram(pDf, 'BinWidth', 0.5);
+title('HTdistance Histogram in rEquilibrium for N200-A2000-a20-l1-r0.02');
 xc = xlim;
 xl = xc(1)*0.2+xc(2)*0.8;
 yc = ylim;
@@ -38,7 +38,7 @@ yl1 = yc(1)*0.17+yc(2)*0.83;
 yl2 = yc(1)*0.23+yc(2)*0.77;
 text(xl,yl1,strcat('mean=',num2str(mean(pDf))),'Color','red','FontSize',12);
 text(xl,yl2,strcat('var=',num2str(var(pDf))),'Color','red','FontSize',12);
-filename = strcat(fpathN, 'rEquil-D-Hist-N',num2str(fnode),'-t',num2str(ftwist),'-a',num2str(fa),'-l',num2str(fL),'-r',num2str(fangle),'.png');
+filename = 'rEquil-D-Hist-N200-A2000-a20-l1-r0.02.png';
 parsaveas(gcf, filename,'png');
 close gcf;
 
@@ -70,7 +70,7 @@ hold on
 plot(x,yCalc);
 xlabel('t')
 ylabel('log(Autocorrelation)')
-title(strcat('Autocorrelation to find relaxation tau(',fname,')'));
+title('Autocorrelation to find relaxation tau(     )');
 legend('Data','Slope & Intercept','Location','best');
 grid on
 
@@ -82,7 +82,5 @@ yc = ylim;
 yl = yc(1)*0.2+yc(2)*0.8;
 text(xl,yl,strcat('tau=',num2str(tau)),'Color','red','FontSize',12);
 
-parsaveas(gcf, strcat(fpathN, fname, '.png'),'png');
+parsaveas(gcf, '  .png','png');
 close gcf;
-
-save(strcat(fpathN, fname, '.txt'), 'tau', '-ascii');
