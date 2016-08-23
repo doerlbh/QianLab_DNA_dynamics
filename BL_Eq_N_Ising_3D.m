95f8cf74f1b98a15e5429c48ff67bcd1e44e33e7
doerlbh <doerlbh@gmail.com>
2016-08-23 15:04:46 -0700

    update code

--- BL_Eq_N_Ising_3D.m
+++ BL_Eq_N_Ising_3D.m
@@ -10,37 +10,44 @@ close all;
 
 %% Initialization
 
-rng(123);                 % randomizer
+rng(100);                 % randomizer
 
-trial = 200;             % trials
-twist = 2000;             % change of set state changes
+trial = 2000;             % trials
+twist = 1000;             % change of set state changes
 % node = 500;               % nodes of rigid polymer
-AutoT = 1000;             % autocorrelation run time
+AutoT = 4000;             % autocorrelation run time
 
 global pathN;
 % pathN = '/Users/sunnylinL/Dropbox/Sim1/data/data_20160817/';
 % pathN = '/gscratch/stf/sunnylin/other/sim/data/';
-pathN = '/Users/DoerLBH/Dropbox/git/QianLab_DNA_dynamics/data/Loop_20160822/';
+pathN = '/Users/DoerLBH/Dropbox/git/QianLab_DNA_dynamics/data/Angle_20160823/';
 
 system(['mkdir ' pathN]);
 
-angle = 0.05;            % in rad, angle changed in each twist
+% angle = 0.02;            % in rad, angle changed in each twist
 L = 1;                  % length of each segment of rigid polymer
 a = 20;                 % threshold to form loop
 
 Hc = 1.0;   % in unit of kT, energy level of cis rigid configuration
 Ht = 0.9;   % in unit of kT, energy level of trans rigid configuration
 
+
+for ang = 1:20
 % parfor it = 1:5
-it = 1;
-% for it = 1:5
+% it = 1;
+for it = 1:10
     
+    angle = ang*0.02;
     node = it*100;
     
     %% Main functions for Equilibrium
     
-%     [finPr, stPr, pEfr, pDfr] = EquilSimulationRandpoly(node, trial, twist, pathN, a, L, angle, Hc, Ht);
+    [finPr, stPr, pEfr, pDfr] = EquilSimulationRandpoly(node, trial, twist, pathN, a, L, angle, Hc, Ht);
     
-    [NfinPr, NstPr, NpEfr, NpDfr] = EquilNatpoly(node, trial, twist, pathN, a, L, angle, Hc, Ht);
+%     [NfinPr, NstPr, NpEfr, NpDfr] = EquilNatpoly(node, trial, twist, pathN, a, L, angle, Hc, Ht);
         
-    [finP,stP,pTf,pEf,pDf] = loopAutorun(node,NfinPr,trial,pathN,a,L,angle,Hc,Ht);
+%     [finP,stP,pTf,pEf,pDf] = loopAutorun(node,NfinPr,trial,pathN,a,L,angle,Hc,Ht);
+    
+end
+
+end
